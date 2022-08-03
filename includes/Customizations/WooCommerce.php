6d7f539abfa356747dbf9141926c4d4a62970d0a
Matias Saggiorato <matiassaggiorato@gmail.com>
2022-08-03 12:56:33 -0300

    Disable ACF extension and add WooCommerce basic customization class

---
+++ includes/Customizations/WooCommerce.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * WooCommerce Hooks
+ *
+ * @package     WoorricaneHelper/Customizations
+ * @version     1.0.0
+ */
+
+namespace WoorricaneHelper\Customizations;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * WooCommerce Class.
+ */
+class WooCommerce {
+
+	/**
+	 * Hook in methods.
+	 *
+	 * @return void
+	 */
+	public static function hooks() {
+	}
+}
--- includes/Main.php
+++ includes/Main.php
@@ -85,7 +85,7 @@ final class Main {
 		// Common includes.
 		Block::hooks();
 
-		Customizations\ACF::hooks();
+		Customizations\WooCommerce::hooks();
 
 		// Set up localisation.
 		self::load_plugin_textdomain();
