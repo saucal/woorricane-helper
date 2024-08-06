a1795b9035874f4ea142e84be8d16b801c513608
Matias Saggiorato <matiassaggiorato@gmail.com>
2024-08-06 18:27:17 -0300

    Simulate emails being sent

--- includes/DisableEmails.php
+++ includes/DisableEmails.php
@@ -23,7 +23,13 @@ class DisableEmails {
 	 * @return void
 	 */
 	public static function hooks() {
-		add_action( 'pre_wp_mail', '__return_true' );
+		add_action( 'pre_wp_mail', array( __CLASS__, 'soft_sending' ), \PHP_INT_MIN );
+	}
+
+	public static function soft_sending() {
+		// Sleep for a random number of miliseconds between 200 and 1000.
+		usleep( wp_rand( 200000, 1000000 ) );
+		return true;
 	}
 
 }
